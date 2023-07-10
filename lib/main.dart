2816a00e01a13cd70ed0e9848cf39fb660c4778d
Mini <oldmacmini@Olds-Mac-mini.local>
2023-07-10 11:24:34 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:connectivity_wrapper/connectivity_wrapper.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ConnectivityAppWrapper(
+
+      app: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
+        builder: (buildContext, widget) {
+          return ConnectivityWidgetWrapper(
+            disableInteraction: true,
+            child: widget!,
+          );
+        },
+      )
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Connectivity Demo"),),
+      body: const Center(
+        child: Text("App Body"),
+      ) ,
+    );
+  }
+}
+
